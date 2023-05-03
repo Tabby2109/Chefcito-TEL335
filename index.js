cea9e6a06f3fa12e0d2750f734ec5f084dcc9dd9
Ryshadio <rodrigo.cvow@gmail.com>
2023-05-02 22:03:52 -0400

    conexión mongodb

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require ("mongoose");
 
-app.use(express.json())
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const database = module.exports = () => {
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    res.json({ success: true })
-})
+    const connectionParams = {
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:3000`))
+        useNewUrlParser: true,
+
+        useUnifiedTopology: true,
+
+    }
+
+    try {
+
+        mongoose.connect(
+            
+            'mongodb://Roshar:XCACrX3siiL9WoUJ@ac-wvk5edf-shard-00-00.j4xhw9s.mongodb.net:27017,ac-wvk5edf-shard-00-01.j4xhw9s.mongodb.net:27017,ac-wvk5edf-shard-00-02.j4xhw9s.mongodb.net:27017/?ssl=true&replicaSet=atlas-147a0g-shard-0&authSource=admin&retryWrites=true&w=majority',
+            
+            connectionParams
+            
+            );
+
+        console.log('Se conecto compare mio');
+
+    } catch (error) {
+
+        console.log(error);
+
+        console.log("fallo la conexion compare mio");
+
+    }
+
+}
+
+database();
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:3000`));
