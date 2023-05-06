768ecd689790016311cee5085ec10d5c106f52f9
Ryshadio <rodrigo.cvow@gmail.com>
2023-05-06 13:32:16 -0400

    Pequeño ajuste
    Cambie la contraseña de pepe porque no podiamos tener poto para la entrega xd. Tambien cambie un mensaje de ingles a español para que sea mas como en casa

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use('/api', userRoutes);
 
 
 app.get('/', (req,res) => {
-    res.send("welcome to my API");
+    res.send("Bienvenido, Rey");
 });
 
 const database = module.exports = async () => {
